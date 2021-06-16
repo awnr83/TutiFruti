441a492d0d1a6ec260bccd583dd8ca5a71237aa8
awnr83 <awnr83@gmail.com>
2021-06-16 14:30:23 -0300

    Temporizador y vibracion
    Se agrego temporizador y vibración

--- app/src/main/java/com/example/tutifruti/game/GameFragment.kt
+++ app/src/main/java/com/example/tutifruti/game/GameFragment.kt
@@ -1,11 +1,16 @@
 package com.example.tutifruti.game
 
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.text.format.DateUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -29,13 +34,33 @@ class GameFragment : Fragment() {
         binding.gameViewModel= viewModel
         binding.lifecycleOwner=this
 
+        viewModel.temp.observe(viewLifecycleOwner, Observer { newTemp->
+            binding.textViewTiempo.text= DateUtils.formatElapsedTime(newTemp)
+            binding.invalidateAll()
+        })
+
+        viewModel.eventVibracion.observe(viewLifecycleOwner, Observer { vib->
+            if(vib != GameViewModel.Vibracion.NO_BUZ){
+                vibracion(vib.parametro)
+                viewModel.terminoVibracion()
+            }
+        })
+
         viewModel.eventFin.observe(viewLifecycleOwner, Observer {termino->
             if(termino) { //si esta en true termino
                 findNavController(this).navigate(GameFragmentDirections.actionGameFragmentToFinFragment())
                 viewModel.terminoCompleto() //con esto se termina de cerrar
             }
         })
-
         return binding.root
+    }
+    private fun vibracion(parametro: LongArray){
+        val vib= activity?.getSystemService<Vibrator>()
+        vib.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                vib?.vibrate(VibrationEffect.createWaveform(parametro, -1))
+             else
+                vib?.vibrate(parametro, -1)
+        }
     }
 }
--- app/src/main/java/com/example/tutifruti/game/GameViewModel.kt
+++ app/src/main/java/com/example/tutifruti/game/GameViewModel.kt
@@ -1,19 +1,48 @@
 package com.example.tutifruti.game
 
+import android.os.CountDownTimer
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+//constantes para vibracion
+private val PANICO_BUZ_P= longArrayOf(0,200)
+private val NO_BUZ_P= longArrayOf(0)
+
 class GameViewModel: ViewModel() {
+
+enum class Vibracion(val parametro: LongArray){
+    PANIC(PANICO_BUZ_P),
+    NO_BUZ(NO_BUZ_P)
+}
+
+//temporizador
+    //valores para el temporizador
+    companion object{
+        private val INICIO=    0L
+        private val SEG=    1000L
+        private val PANICO_SEG= 10000L
+        private val TEMP=  60000L
+    }
+    private lateinit var temporizador:CountDownTimer
 
 //variables
     private lateinit var listaLetras: MutableList<String>
+
 //encapsulamiento LIVE DATA
     //letra
     private val _letra= MutableLiveData<String>()
     val letra: LiveData<String>
         get()=_letra
+    //temporizador
+    private val _temp= MutableLiveData<Long>()
+    val temp: LiveData<Long>
+        get()=_temp
+    //vibracion
+    private val _eventVibracion= MutableLiveData<Vibracion>()
+    val eventVibracion: LiveData<Vibracion>
+        get()=_eventVibracion
     //evento termino
     private val _eventFin= MutableLiveData<Boolean>()
     val eventFin: LiveData<Boolean>
@@ -21,6 +50,7 @@ class GameViewModel: ViewModel() {
 
     init {
         _eventFin.value=false //se inica false porque todavia no termino
+        iniciarTemporizador()
         resetLista()
         siguienteLetra()
     }
@@ -28,16 +58,32 @@ class GameViewModel: ViewModel() {
         listaLetras= mutableListOf("A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","W","X","Y","Z")
         listaLetras.shuffle()
     }
-
+    private fun iniciarTemporizador(){
+        temporizador= object:CountDownTimer(TEMP, SEG) {
+            override fun onTick(millisUntilFinished: Long) {
+                if(millisUntilFinished <= PANICO_SEG) {         //solamente vibra cuando llega al panico
+                    _eventVibracion.value= Vibracion.PANIC
+                }
+                _temp.value= (millisUntilFinished / SEG)
+            }
+            override fun onFinish() {
+                _temp.value= INICIO
+            }
+        }
+        temporizador.start()
+    }
     fun siguienteLetra(){
-        if(listaLetras.size>0)
-            _letra.value= listaLetras.removeAt(0)
-        else{
+        if(listaLetras.size>0) {
+            iniciarTemporizador()
+            _letra.value = listaLetras.removeAt(0)
+        }else{
             _eventFin.value=true //como termino se pone en true
         }
     }
-
     fun terminoCompleto(){
         _eventFin.value= false //funciona como un candado ya no entra
+    }
+    fun terminoVibracion(){
+        _eventVibracion.value= Vibracion.NO_BUZ
     }
 }
